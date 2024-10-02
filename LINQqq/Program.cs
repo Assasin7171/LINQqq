b5d53aa4f2ba932244068ff0cd000329c03d3068
Konrad Bojanecki <KBojanecki@gmail.com>
2024-10-02 18:07:26 +0200

    .

--- LINQqq/Program.cs
+++ LINQqq/Program.cs
@@ -7,20 +7,95 @@ namespace LINQqq
     {
         static void Main(string[] args)
         {
-            string csvPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"googleplaystore1.csv");
+            string csvPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "googleplaystore1.csv");
             var googleApps = LoadGoogleAps(csvPath);
 
             //Display(googleApps);
             //GetData(googleApps);
             //ProjectData(googleApps);
             //DivideData(googleApps);
-            OrderData(googleApps);
+            //OrderData(googleApps);
+            //DataSetOperation(googleApps);
+            //DataVerification(googleApps);
+            //GroupData(googleApps);
+            GroupDataOperations(googleApps);
+        }
+
+        private static void GroupDataOperations(List<GoogleApp> googleApps)
+        {
+            var categoryGroup = googleApps
+                .GroupBy(g => g.Category);
+
+            foreach (var group in categoryGroup)
+            {
+                var averageReviews = group.Average(g => g.Reviews);
+                var minReviews = group.Min(g => g.Reviews);
+                var maxReviews = group.Max(g => g.Reviews);
+                var reviewsCount = group.Sum(g => g.Reviews);
+
+                var allAppsFromGroupHaveRatingOfThree = group.All(g => g.Rating > 3.0);
+
+                Console.WriteLine($"Category {group.Key}");
+                Console.WriteLine($"Avrage reviews: {averageReviews}");
+                Console.WriteLine($"Min reviews: {minReviews}");
+                Console.WriteLine($"Max reviews: {maxReviews}");
+                Console.WriteLine($"Counted reviews: {reviewsCount}");
+                Console.WriteLine($"allAppsFromGroupHaveRatingOfThree {allAppsFromGroupHaveRatingOfThree}");
+                Console.WriteLine("\n");
+            }
+        }
+
+        private static void GroupData(List<GoogleApp> googleApps)
+        {
+            var categoryGroup = googleApps.GroupBy(a => a.Category);
+
+            var group = categoryGroup.First(g => g.Key == Category.ART_AND_DESIGN).ToList();
+            Display(group);
+        }
+
+        private static void DataVerification(List<GoogleApp> googleApps)
+        {
+            var dataSet = googleApps.Where(a => a.Category == Category.WEATHER)
+                .All(a => a.Reviews > 20);
+            Console.WriteLine(dataSet);
+
+            var anyOperatorResult = googleApps.Where(a => a.Category == Category.WEATHER)
+                .Any(a => a.Reviews > 4_000_000);
+            Console.WriteLine(anyOperatorResult);
+        }
+
+        private static void DataSetOperation(List<GoogleApp> googleApps)
+        {
+            var paidAppsCategories = googleApps.Where(a => a.Type == Type.Paid)
+                .Select(a => a.Category)
+                .Distinct();
+
+            //Console.WriteLine($"Paid apps categories: {string.Join(",", paidAppsCategories)}");
+
+            var setA = googleApps.Where(a => a.Rating > 4.7 && a.Type == Type.Paid && a.Reviews > 1000);
+            var setB = googleApps.Where(a => a.Name.Contains("Pro") && a.Rating > 4.6 && a.Reviews > 10000);
+
+            //Display(setA);
+            //Console.WriteLine("\n");
+            //Display(setB);
+
+            //scalamy dwa zbiory bez duplikatów
+            //var setAB = setA.Union(setB);
+            //Display(setAB);
+
+            //z dwóch zbiorów dostajemy tylko wspólne dane
+            //var setAB = setA.Intersect(setB);
+            //Display(setAB);
+
+            //z dwóch zbiorów dostajemy tylko dane które znajdują się z zbiorze A i nie występują w zbiorze B
+            //var setAB = setA.Except(setB);
+            //Display(setAB);
         }
 
         private static void OrderData(List<GoogleApp> googleApps)
         {
             var hightRatedBeautyApps = googleApps.Where(app => app.Rating > 4.6 & app.Category == Category.BEAUTY)
-                .OrderByDescending(app=> app.Rating).ThenByDescending(app=> app.Name);
+                .OrderByDescending(app => app.Rating).ThenByDescending(app => app.Name);
 
             Display(hightRatedBeautyApps);
         }
@@ -52,7 +127,7 @@ namespace LINQqq
                 Name = app.Name,
                 Reviews = app.Reviews
             });
-            
+
             var anonymousDtos = hightRatedBeautyApps.Select(app => new
             {
                 Name = app.Name,
@@ -64,7 +139,7 @@ namespace LINQqq
                 Console.WriteLine($"{dto.Name}: {dto.Reviews}");
             }
 
-            var genres = hightRatedBeautyApps.SelectMany(app=> app.Genres);
+            var genres = hightRatedBeautyApps.SelectMany(app => app.Genres);
             //Console.WriteLine(string.Join(":", genres));
 
             //Console.WriteLine(string.Join(", ",hightRatedBeautyAppsNames));
